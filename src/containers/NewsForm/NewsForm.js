1fe9595254758104a7c77e9606c7196707f370d1
Hao Nguyen <doibanchan1997@gmail.com>
2020-10-01 13:23:47 +0700

    add something

--- src/containers/NewsForm/NewsForm.js
+++ src/containers/NewsForm/NewsForm.js
@@ -7,8 +7,8 @@ import './NewsForm.css';
 import *as  modalAction from './../../actions/modal';
 import *as newsAction from './../../actions/news'
 const NewsForm = (props) =>{
-  const {modalActionCreators, newsActionCreators, initialValues} = props;
-  console.log(initialValues);
+  const {modalActionCreators, newsActionCreators, newsEditing} = props;
+  console.log(newsEditing);
   const {hiddenModal} = modalActionCreators;
   const {handleSubmit} = props;
   const handleSubmitForm = (data) =>{
@@ -20,20 +20,44 @@ const NewsForm = (props) =>{
         <form onSubmit={handleSubmit(handleSubmitForm)} action="/action_page.php">
         <div className="form-group">
           <label htmlFor="text">Tieu De Bai Viet:</label>
-          <Field className="form-control" name="titleNews" component="input" type="text"/>
+          <Field 
+            value={newsEditing.titleNews ? newsEditing.titleNews: ''} 
+            className="form-control" 
+            name="titleNews" 
+            component="input" 
+            type="text"
+            />
         </div>
         <div className="form-group">
           <label htmlFor="text">Tac Gia:</label>
-          <Field className="form-control" name="authorNews" component="input" type="text"/>
+          <Field 
+          value={newsEditing?newsEditing.authorNews: ''} 
+          className="form-control" 
+          name="authorNews" 
+          component="input" 
+          type="text"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="text">Ten Bai Viet:</label>
-          <Field className="form-control" name="nameNews" component="input" type="text"/>
+          <Field 
+            value={newsEditing?newsEditing.nameNews : ''} 
+            className="form-control" 
+            name="nameNews" 
+            component="input" 
+            type="text"
+            />
         </div>
         
         <div className="form-group">
           <label htmlFor="text">Bai Viet</label>
-          <Field className="form-control" name="news" component={renderEditor} type="text"/>
+          <Field 
+            value={newsEditing?newsEditing.news : ''} 
+            className="form-control" 
+            name="news" 
+            component={renderEditor} 
+            type="text"
+          />
         </div>
         <div className="modal-footer">
               <button type="button" onClick={hiddenModal} className="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -46,7 +70,12 @@ const NewsForm = (props) =>{
 const mapStateToProps = state =>{
   return {
     newsEditing: state.news.newsEditing,
-    initialValues: state.news.newsEditing
+    initialValues: {
+      titleNews : state.news.newsEditing? state.news.newsEditing.titleNews: null,
+      nameNews : state.news.newsEditing? state.news.newsEditing.nameNews : null,
+      authorNews: state.news.newsEditing? state.news.newsEditing.authorNews: null,
+      news : state.news.newsEditing? state.news.newsEditing.news : null
+    }
 
   }
 }
@@ -60,6 +89,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
 const FORM_NAME = 'NEWS_MANAGEMENT'
   const withReduxForm = reduxForm({
-    form: FORM_NAME
+    form: FORM_NAME,
+    endableREinitialize: true
   })
 export default compose(withReduxForm, withConnect)(NewsForm);
